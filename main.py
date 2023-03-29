cd4a2040a9e270743e8db7e228f95bc4e2530856
michellestapp <mstapp@rocketmail.com>
2023-03-29 17:05:26 -0500

    feat: added Fibonacci calc - from 1 and user input

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # # Write a method that determines if a number is happy or sad
 
 def happy_numbers():
-    
+
     hold_num_index = 0
     hold_nums = []
     happy = 'False'
@@ -25,8 +25,8 @@ def happy_numbers():
 
         for char in number:
             sum += int(char) * int(char)
-        
-        
+
+
         if sum == 1:
             print(f" {input_number} is a happy 😊 number!")
             happy = 'True'
@@ -39,7 +39,7 @@ def happy_numbers():
             hold_nums.append(int(sum))
             hold_num_index += 1
             sum = 0
-            
+    
 happy_numbers()
 
 # Task 2: Prime Numbers
@@ -53,14 +53,48 @@ def is_prime():
     for num in range(2,101):
         for divisor in range(2, num-1):
             is_divisible = num % divisor
-            
+    
             if is_divisible == 0:
                 prime = "False"
-        
+
         if prime == "True":
             print(num)
         prime = "True"
-    
+
 
 is_prime()
    
+# Task 3: Fibonacci
+# A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+# Write a method that does the Fibonacci sequence starting at 1
+
+def fibonacci_from_1():
+    fib_num = [1,1]
+
+    for index in range(0,50):
+        if index == 0 or index == 1:
+            print(fib_num[index])
+        else:
+            num = fib_num[index -1] + fib_num[index-2]
+            fib_num.append(num)
+            print(fib_num[index])
+
+fibonacci_from_1()
+
+# Fibonacci Sequence from user input
+
+def fibonacci_from_user():        
+    fib_num_alt = []
+
+    number = input(" Enter a number to start the Fibonacci Sequence: ")
+    user_number = int(number)
+    for index in range(0,50):
+        if index == 0 or index == 1:
+            fib_num_alt.append(user_number)
+            print(fib_num_alt[index])
+        else:
+            num = fib_num_alt[index -1] + fib_num_alt[index-2]
+            fib_num_alt.append(num)
+            print(fib_num_alt[index])
+
+fibonacci_from_user()
